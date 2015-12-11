df1496af4201ced9df394fb3de327185c3e7fe70
Harris, Michael <micharris@gannett.com>
2015-12-11 12:08:04 -0500

    changed file path

--- query.py
+++ query.py
@@ -17,9 +17,13 @@ end = start + 120
 #system.load.1 = The average system load over one minute.
 query_cpu = 'system.cpu.idle{farm:caproxy,environment:development,farm_role:caproxy-app} by {host}' 
 results = api.Metric.query(start=start - 120, end=end, query=query_cpu)
-print json.dumps(results)
+
+with open('/opt/tcagents/tc_cs_agent/temp/agentTmp/cpu.json', 'w') as file_:
+    file_.write(json.dumps(results))
 
 #system.mem.used = The amount of RAM in use shown as byte
 query_memory = 'system.mem.used{farm:caproxy,environment:development,farm_role:caproxy-app} by {host}'
 results = api.Metric.query(start=start - 120, end=end, query=query_memory)
-print json.dumps(results)
+
+with open('/opt/tcagents/tc_cs_agent/temp/agentTmp/memory.json', 'w') as file_:
+    file_.write(json.dumps(results))
